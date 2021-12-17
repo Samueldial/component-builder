e252a708b594be0d24ef4db6ce18e3c429f48176
Lusca Lima <lusca.lima@outlook.com>
2021-12-16 23:42:17 -0300

    chore: add prettier config

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <h1>HelloWorld</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequuntur vero possimus sed doloribus laborum ducimus corporis necessitatibus quibusdam repellendus id, exercitationem beatae reiciendis! Debitis magnam asperiores harum esse recusandae.</p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+    consequuntur vero possimus sed doloribus laborum ducimus corporis
+    necessitatibus quibusdam repellendus id, exercitationem beatae reiciendis!
+    Debitis magnam asperiores harum esse recusandae.
+  </p>
 </template>
 
-<script setup>
-
-</script>
-
-<style>
-
-</style>
+<script setup></script>

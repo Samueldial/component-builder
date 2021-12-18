a7deb00ecb7537e65d35d37146f575d3fa9f630c
Lusca Lima <lusca.lima@outlook.com>
2021-12-18 12:22:07 -0300

    feat: configure brand fonts and colors

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from "vue"
 import { autoComponentsRegister } from "./autoImport"
 import router from "./router"
 import App from "./App.vue"
-import "./index.css"
+
+import "./assets/css/fonts.css"
+import "./assets/css/tailwind.css"
 
 const app = createApp(App)
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,29 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          dark: "#312f2f",
+          gray: "#a7a7ab",
+          lightGray: "#d9d8d8",
+          light: "#f8fafc",
+          red: "#e31836",
+          orange: "#df6524",
+          gree: "#7abe35",
+          blue: "#305092"
+        }
+      },
+      fontFamily: {
+        light: ["JetBrainsMonoLight"],
+        regular: ["JetBrainsMonoRegular"],
+        medium: ["JetBrainsMonoMedium"],
+        bold: ["JetBrainsMonoBold"]
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }

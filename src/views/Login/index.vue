447795f4695ca54ac20caa3a1bbe3e5684f99bbe
Samueldial <samueldial@gmail.com>
2021-12-17 22:12:03 -0300

    feat: start login view

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,16 @@
 <template>
- <h1>Login</h1>
+  <div class="flex w-screen h-screen">
+    <div class="flex flex-1 p-20">
+      <div class="w-3/4 self-end">
+        <h1 class="text-2xl">build beautiful components based on <span class="font-light">Tailwind CSS</span> classes</h1>
+      </div>
+    </div>
+    <div class="flex flex-1 p-20">
+      <div class="w-7/12 mx-auto"><img src="../../assets/logo.svg" alt="Blaker's Logo"></div>
+      <form action="">
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
